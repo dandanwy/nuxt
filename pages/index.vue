828ffdbee1b369301e92d867ab799af711727b58
LAPTOP-NJTJL3I7\wy <wangyan@gzrqian.com>
2018-07-31 08:47:52 +0800

    初建nuxt项目，完成路由传参

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <!-- <li><a href="/">HOME</a></li>
+      <li><a href="/about">ABOUT</a></li>
+      <li><a href="/news">NEWS</a></li> -->
+      <li><nuxt-link :to="{name:'index'}">HOME</nuxt-link></li>
+      <li><nuxt-link :to="{name:'about'}">ABOUT</nuxt-link></li>
+      <li><nuxt-link :to="{name:'news',params:{newsId:3360}}">NEWS</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+ 
+<script>
+ 
+export default {
+  components: {
+   
+  }
+}
+</script>
+ 
+<style>
+ 
+</style>
